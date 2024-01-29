9e30406c1690273124f5035bdc8d0245bb7ecee0
Artyom Abramyan <artemabramyan7@icloud.com>
2024-01-29 22:36:46 +0300

    Organize admin for website app

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Inbox, InboxFile, Project, ProjectLog, Space, Task
 
-# Register your models here.
+
+class InboxFileInlineAdmin(admin.StackedInline):
+    model = InboxFile
+
+
+@admin.register(Inbox)
+class InboxAdmin(admin.ModelAdmin):
+    inlines = [InboxFileInlineAdmin]
+
+
+class ProjectLogInlineAdmin(admin.StackedInline):
+    model = ProjectLog
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectLogInlineAdmin]
+
+
+@admin.register(Space)
+class SpaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
