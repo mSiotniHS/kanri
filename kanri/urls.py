325ad88692d55ff6c2d6bbea0ede74fcd4772d7b
Artyom Abramyan <artemabramyan7@icloud.com>
2024-02-29 23:10:26 +0300

    Add website's urls to the project

--- kanri/urls.py
+++ kanri/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', include('website.urls')),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
